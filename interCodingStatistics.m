b5064453e91f4a3cfe41638635351a6e18a0d41d
moisesejimenezg <moisesejimenezg@gmail.com>
2015-01-22 01:14:50 +0100

    <MINOR>: Corrected minor errors for histogram and summation computations.

--- interCodingStatistics.m
+++ interCodingStatistics.m
@@ -29,8 +29,8 @@ function [intra_mv_histogram , intra_mv_histogram_sum, intra_ef_histogram,intra_
     [ycbcr_predicted_frame,motion_matrix]=InterEncodeFrame(macroblock_dim,mv_search_range,frame_01,frame_00);
     ycbcr_prediction_error_frame = frame_01 - ycbcr_predicted_frame;
     ycbcr_intra_encoded_error_frame = preEncodeIntraProcess(ycbcr_prediction_error_frame,Q);
-    intra_mv_histogram = hist(motion_matrix,-mv_search_range:mv_search_range);
-    intra_mv_histogram_sum = sum(intra_mv_histogram);
+    intra_mv_histogram = hist(motion_matrix(:),-mv_search_range:mv_search_range);
+    intra_mv_histogram_sum = sum(sum(intra_mv_histogram));
     intra_ef_histogram = hist(ycbcr_intra_encoded_error_frame,ef_lowerbound:ef_upperbound);
     intra_ef_histogram_sum = sum(intra_ef_histogram);
 end
