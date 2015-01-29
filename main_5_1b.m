1f62cd9b1f676005cd852dd78a80d70376f26db7
moisesejimenezg <moisesejimenezg@gmail.com>
2015-01-30 00:48:08 +0100

    <UPDATE>: Modified to read from memory new huffman coding tables and include B frames.

--- main_5_1b.m
+++ main_5_1b.m
@@ -5,7 +5,7 @@ clearvars
 %%%%%%%%%%%% VARIABLE DECLARATION %%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 GOP_enabled = true;
-Q = 1;
+Q = 5;
 ycbcr_lowerbound = -128;
 er_lowerbound = -255;
 upperbound = 260;
@@ -72,12 +72,15 @@ clear path_index
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%% LOAD HUFFMAN TABLE %%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Intra matrices
 intra_binary_tree = load('video_codec/huffman_tables/intra_binary_tree','-mat');
 intra_binary_tree = intra_binary_tree.intra_binary_tree;
 intra_bin_code = load('video_codec/huffman_tables/intra_bin_code','-mat');
 intra_bin_code = intra_bin_code.intra_bin_code;
 intra_codelengths = load('video_codec/huffman_tables/intra_codelengths','-mat');
 intra_codelengths = intra_codelengths.intra_codelengths;
+% P inter matrices
 inter_binary_tree_ef = load('video_codec/huffman_tables/inter_binary_tree_ef','-mat');
 inter_binary_tree_ef = inter_binary_tree_ef.inter_binary_tree_ef;
 inter_bin_code_ef = load('video_codec/huffman_tables/inter_bin_code_ef','-mat');
@@ -90,6 +93,19 @@ inter_bin_code_mv = load('video_codec/huffman_tables/inter_bin_code_mv','-mat');
 inter_bin_code_mv = inter_bin_code_mv.inter_bin_code_mv;
 inter_codelengths_mv = load('video_codec/huffman_tables/inter_codelengths_mv','-mat');
 inter_codelengths_mv = inter_codelengths_mv.inter_codelengths_mv;
+% P inter matrices
+inter_b_binary_tree_ef = load('video_codec/huffman_tables/inter_b_binary_tree_ef','-mat');
+inter_b_binary_tree_ef = inter_b_binary_tree_ef.inter_b_binary_tree_ef;
+inter_b_bin_code_ef = load('video_codec/huffman_tables/inter_b_bin_code_ef','-mat');
+inter_b_bin_code_ef = inter_b_bin_code_ef.inter_b_bin_code_ef;
+inter_b_codelengths_ef = load('video_codec/huffman_tables/inter_b_codelengths_ef','-mat');
+inter_b_codelengths_ef = inter_b_codelengths_ef.inter_b_codelengths_ef;
+inter_b_binary_tree_mv = load('video_codec/huffman_tables/inter_b_binary_tree_mv','-mat');
+inter_b_binary_tree_mv = inter_b_binary_tree_mv.inter_b_binary_tree_mv;
+inter_b_bin_code_mv = load('video_codec/huffman_tables/inter_b_bin_code_mv','-mat');
+inter_b_bin_code_mv = inter_b_bin_code_mv.inter_b_bin_code_mv;
+inter_b_codelengths_mv = load('video_codec/huffman_tables/inter_b_codelengths_mv','-mat');
+inter_b_codelengths_mv = inter_b_codelengths_mv.inter_b_codelengths_mv;
 %% INTRA CODING OF FIRST FRAME
 
 
@@ -100,14 +116,15 @@ inter_codelengths_mv = inter_codelengths_mv.inter_codelengths_mv;
 start = cputime;
 for i = 1:21
     current_frame = frames{i};
-    %% INTRA CODING
     if gop(i) == 'I'
+    %% I FRAME
         ycbcr_reference_frame = ictRGB2YCbCr(current_frame);
         [ycbcr_decoded_frame,intra_encoded_frame] = IntraEncodeFrame(ycbcr_reference_frame,intra_binary_tree, intra_bin_code, intra_codelengths, Q,ycbcr_lowerbound);
         psnrs(1) = calculatePSNR(8,current_frame,ictYCbCr2RGB(ycbcr_decoded_frame));
         bitrates(1) = calculateBitrate(frames{1},intra_encoded_frame);
         ycbcr_i_frame = ycbcr_decoded_frame;
     elseif gop(i) == 'P'
+        %% P FRAMES
         ycbcr_current_frame = ictRGB2YCbCr(current_frame);
         ycbcr_reference_frame = ycbcr_decoded_frame;
         %% MOTION ESTIMATION
@@ -118,34 +135,35 @@ for i = 1:21
         ycbcr_prediction_error_frame=ycbcr_current_frame-ycbcr_predicted_frame;
 
         %% INTRA CODING OF ERROR FRAME
-        [ycbcr_decoded_error_frame,huffman_error_stream] = IntraEncodeFrame(ycbcr_prediction_error_frame,inter_binary_tree_ef,inter_bin_code_ef,inter_codelengths_ef, Q,er_lowerbound);
+        [ycbcr_decoded_error_frame,huffman_error_stream]=IntraEncodeFrame(ycbcr_prediction_error_frame,inter_binary_tree_ef,inter_bin_code_ef,inter_codelengths_ef,Q,er_lowerbound);
 
         %% HUFFMAN CODING OF MV
-        [huffman_mv] = enc_huffman_new(motion_matrix+mv_search_range+1,inter_bin_code_mv,inter_codelengths_mv);
+        [huffman_mv]=enc_huffman_new(motion_matrix+mv_search_range+1,inter_bin_code_mv,inter_codelengths_mv);
 
         %% DECODING
-        decoded_mv = dec_huffman_new(huffman_mv,inter_binary_tree_mv,max(size(motion_matrix(:)))) - 4 - 1;
-        new_decoded_image_mv_00 = decoded_mv(1:36*44);
-        new_decoded_image_mv_01 = decoded_mv(36*44+1:2*36*44);
-        reshaped_mv(:,:,1) = reshape (new_decoded_image_mv_00,36,44);
-        reshaped_mv(:,:,2) = reshape (new_decoded_image_mv_01,36,44);
+        decoded_mv=dec_huffman_new(huffman_mv,inter_binary_tree_mv,max(size(motion_matrix(:))))-4-1;
+        new_decoded_image_mv_00=decoded_mv(1:36*44);
+        new_decoded_image_mv_01=decoded_mv(36*44+1:2*36*44);
+        reshaped_mv(:,:,1)=reshape(new_decoded_image_mv_00,36,44);
+        reshaped_mv(:,:,2)=reshape(new_decoded_image_mv_01,36,44);
 
         %% MOTION COMPENSATION
-
         clear ycbcr_predicted_frame
         ycbcr_predicted_frame = motionCompensation(ycbcr_reference_frame,reshaped_mv,macroblock_dim);
-        %% -------------------- DEBLOCKING FILTERING --------------------------
-        %ycbcr_filtered_frame = DeblockingFilter(ycbcr_predicted_frame,macroblock_dim,Q);
+        
         ycbcr_recovered_frame = ycbcr_predicted_frame + ycbcr_decoded_error_frame;
         if i == 2
             ycbcr_p_0_frame = ycbcr_recovered_frame;
+            psnrs(2) = calculatePSNR(8,current_frame,ictYCbCr2RGB(ycbcr_recovered_frame));
+            bitrates(2) = calculateBitrate(current_frame,[huffman_mv ; huffman_error_stream]);
         elseif i == 3
             ycbcr_p_1_frame = ycbcr_recovered_frame;
+            psnrs(3) = calculatePSNR(8,current_frame,ictYCbCr2RGB(ycbcr_recovered_frame));
+            bitrates(3) = calculateBitrate(current_frame,[huffman_mv ; huffman_error_stream]);
         end
-        psnrs(i + 1) = calculatePSNR(8,current_frame,ictYCbCr2RGB(ycbcr_recovered_frame));
-        bitrates(i + 1) = calculateBitrate(current_frame,[huffman_mv ; huffman_error_stream]);
         ycbcr_decoded_frame = ycbcr_recovered_frame;
     elseif gop(i) == 'B'
+        %% B FRAMES
         ycbcr_current_frame = ictRGB2YCbCr(current_frame);
         if i <= 12
             ycbcr_reference_0_frame = ycbcr_i_frame;
@@ -162,32 +180,27 @@ for i = 1:21
         ycbcr_prediction_error_frame=ycbcr_current_frame-ycbcr_predicted_frame;
 
         %% INTRA CODING OF ERROR FRAME
-        [ycbcr_decoded_error_frame,huffman_error_stream] = IntraEncodeFrame(ycbcr_prediction_error_frame,inter_binary_tree_ef,inter_bin_code_ef,inter_codelengths_ef, Q,er_lowerbound);
+        [ycbcr_decoded_error_frame,huffman_error_stream] = IntraEncodeFrame(ycbcr_prediction_error_frame,inter_b_binary_tree_ef,inter_b_bin_code_ef,inter_b_codelengths_ef, Q,er_lowerbound);
 
         %% HUFFMAN CODING OF MV
-        [huffman_mv] = enc_huffman_new(motion_matrix+mv_search_range+1,inter_bin_code_mv,inter_codelengths_mv);
+        [huffman_mv] = enc_huffman_new(motion_matrix+mv_search_range+1,inter_b_bin_code_mv,inter_b_codelengths_mv);
 
         %% DECODING
-        decoded_mv = dec_huffman_new(huffman_mv,inter_binary_tree_mv,max(size(motion_matrix(:)))) - 4 - 1;
+        decoded_mv = dec_huffman_new(huffman_mv,inter_b_binary_tree_mv,max(size(motion_matrix(:)))) - 4 - 1;
         new_decoded_image_mv_00 = decoded_mv(1:36*44);
         new_decoded_image_mv_01 = decoded_mv(36*44+1:2*36*44);
+        new_decoded_image_mv_02 = decoded_mv(2*36*44+1:3*36*44);
         reshaped_mv(:,:,1) = reshape (new_decoded_image_mv_00,36,44);
         reshaped_mv(:,:,2) = reshape (new_decoded_image_mv_01,36,44);
+        reshaped_mv(:,:,3) = reshape (new_decoded_image_mv_02,36,44);
 
         %% MOTION COMPENSATION
-
         clear ycbcr_predicted_frame
         ycbcr_predicted_frame = BidirectionalMotionCompensation(ycbcr_reference_1_frame,ycbcr_reference_0_frame,reshaped_mv,macroblock_dim);
-        %% -------------------- DEBLOCKING FILTERING --------------------------
-        %ycbcr_filtered_frame = DeblockingFilter(ycbcr_predicted_frame,macroblock_dim,Q);
         ycbcr_recovered_frame = ycbcr_predicted_frame + ycbcr_decoded_error_frame;
-        if i == 2
-            ycbcr_p_0_frame = ycbcr_recovered_frame;
-        elseif i == 3
-            ycbcr_p_1_frame = ycbcr_recovered_frame;
-        end
-        psnrs(i + 1) = calculatePSNR(8,current_frame,ictYCbCr2RGB(ycbcr_recovered_frame));
-        bitrates(i + 1) = calculateBitrate(current_frame,[huffman_mv ; huffman_error_stream]);
+        
+        psnrs(i) = calculatePSNR(8,current_frame,ictYCbCr2RGB(ycbcr_recovered_frame));
+        bitrates(i) = calculateBitrate(current_frame,[huffman_mv ; huffman_error_stream]);
         ycbcr_decoded_frame = ycbcr_recovered_frame;
     end
 end
